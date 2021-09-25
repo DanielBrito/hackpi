04a218e9baa1ea5e33790b655781d44f6afd4fec
danielbrito <daniell-henrique@hotmail.com>
2021-09-25 16:41:33 -0300

    Adjustments

--- hackpi/src/components/BarChart/index.js
+++ hackpi/src/components/BarChart/index.js
@@ -12,22 +12,22 @@ const loadData = (info) => {
     labels: info.data.map((answer) => answer.x),
     datasets: [
       {
-        label: "# Número de Respostas",
+        label: "",
         data: info.data.map((answer) => answer.y),
         backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
+          "rgba(235, 112, 113, 0.2)", // Discordo plenamente
+          "rgba(124, 122, 158, 0.2)", // Discordo
+          "rgba(14, 132, 204, 0.2)", // Neutro
+          "rgba(31, 119, 131, 0.2)", // Concordo
+          "rgba(49, 107, 60, 0.2)", // Concordo plenamente
           "rgba(255, 159, 64, 0.2)",
         ],
         borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
+          "rgba(235, 112, 113, 1)", // Discordo plenamente
+          "rgba(124, 122, 158, 1)", // Discordo
+          "rgba(14, 132, 204, 1)", // Neutro
+          "rgba(31, 119, 131, 1)", // Concordo
+          "rgba(49, 107, 60, 1)", // Concordo plenamente
           "rgba(255, 159, 64, 1)",
         ],
         borderWidth: 1,
@@ -50,17 +50,24 @@ const options = {
 
 const BarChart = () => {
   return (
-    <div id="wrapper">
-      {answers.map((answer, index) => (
-        <div id="chart" key={index}>
-          <div className="header">
-            <h2 className="title">{answer.title}</h2>
-          </div>
-          <Consideration text={"Nossa consideração"} />
-          <Bar data={loadData(answer)} options={options} />
-        </div>
-      ))}
-    </div>
+    <>
+      <div id="wrapper">
+        {answers.map((answer, index) => (
+          <>
+            <div id="chart" key={index}>
+              <div className="header">
+                <h3 className="title">{answer.title}</h3>
+              </div>
+              <Consideration text={"Nossa consideração"} />
+              <Bar data={loadData(answer)} options={options} />
+              <div id="hider"></div>
+              <div id="y-label">Número de respostas</div>
+              <div id="x-label">Tipos de respostas</div>
+            </div>
+          </>
+        ))}
+      </div>
+    </>
   );
 };
 
