926fa62c8bf236ddd42ab11929430b62957db0c9
danielbrito <daniell-henrique@hotmail.com>
2021-09-25 20:48:00 -0300

    Included Development data

--- hackpi/src/views/Development/index.js
+++ hackpi/src/views/Development/index.js
@@ -2,12 +2,16 @@ import React from "react";
 
 import Header from "../../components/Header";
 import Subtitle from "../../components/Subtitle";
+import BarChart from "../../components/BarChart";
+
+import answers from "../../data/sample4.json";
 
 const Development = () => {
   return (
     <>
       <Header />
       <Subtitle text={"Desenvolvimento"} />
+      <BarChart payload={answers} />
     </>
   );
 };
